a84162cde0dd35d99cc914b9fc53dfe2fb1d3a93
RIFAT1287 <92775750+RIFAT1287@users.noreply.github.com>
2024-07-09 16:06:59 +0600

    Update main.py

--- hscript/main.py
+++ hscript/main.py
@@ -1,18 +1,18 @@
 import os
 
-print("Bem vindos ao Hentais Tube Downloader")
-print("\nEsse script foi criado por e42b, acesse o repositório do projeto em https://github.com/e43b/HentaisTube-Downloader")
+print("Welcome to Hentais Tube Downloader")
+print("\nThis script was created by e42b, visit the project repository at https://github.com/e43b/HentaisTube-Downloader")
 
-print("\nPara baixar vários episódios de uma obra digite 1")
-print("Para baixar um episódio específico digite 2")
+print("\nTo download multiple episodes of a series, type 1")
+print("To download a specific episode, type 2")
 
-opcao = input("\nDigite sua opção: ")
+option = input("\nEnter your option: ")
 
-if opcao == "1":
-    # Executar script para baixar todos os episódios de uma obra
-    os.system("python obra.py")
-elif opcao == "2":
-    # Executar script para baixar um episódio específico
-    os.system("python episodio.py")
+if option == "1":
+    # Execute script to download all episodes of a series
+    os.system("python series.py")
+elif option == "2":
+    # Execute script to download a specific episode
+    os.system("python episode.py")
 else:
-    print("Opção inválida. Por favor, escolha 1 ou 2.")
+    print("Invalid option. Please choose 1 or 2.")
